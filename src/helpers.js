808cd8f36d92b397b2248f6e4ad51ff0bd9c72b1
Derek Zhang <derek.zhang0210@gmail.com>
2022-03-27 22:47:56 -0400

    Add some helper functions

--- src/helpers.js
+++ src/helpers.js
@@ -22,6 +22,30 @@ export const getShuffledDeck = () => {
   return deck;
 }
 
+
+const compare = (c1, c2) => {
+  const suit = {
+    'Diamonds': 1, 'Clubs': 2,
+    'Hearts': 3, 'Spades': 4,
+  };
+
+  if(c1.suit != c2.suit){
+    return suit[c1.suit] > suit[c2.suit];
+  }
+
+  const cases = { 'J': 11, 'Q': 12, 'K': 13 }
+  const v1 = (c1.value in cases) ? cases[c1.value] : Number(c1.value);
+  const v2 = (c2.value in cases) ? cases[c2.value] : Number(c2.value);
+
+  return v1 > v2;
+}
+
+export const insertSorted = (card, deck) => {
+  let i = deck.length/2;
+
+  // TODO  
+}
+
 export const getEmptyHand = () => {
   return [ null, null, null, null, null ]
 }
